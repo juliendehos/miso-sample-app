709d3ae4a950e5c643d4dffc151dea9c4a20a3a1
Julien Dehos <dehos@univ-littoral.fr>
2025-05-12 18:27:28 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Miso sample app
+
+## How to build/run
+
+```
+nix develop
+
+./build.sh
+
+http-server public
+```
+
+## Resources
+
+- https://github.com/dmjio/miso
+- https://github.com/tweag/ghc-wasm-miso-examples
+
---
+++ src/Main.hs
@@ -0,0 +1,63 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE CPP               #-}
+
+module Main where
+
+import Miso
+import Miso.String
+import Miso.Lens
+
+-- | Component model state
+newtype Model = Model
+  { _counter :: Int
+  } deriving (Show, Eq)
+
+counter :: Lens Model Int
+counter = lens _counter $ \record field -> record { _counter = field }
+
+-- | Sum type for Component events
+data Action
+  = AddOne
+  | SubtractOne
+  | SayHelloWorld
+  deriving (Show, Eq)
+
+-- | Entry point for a miso application
+main :: IO ()
+main = run (startComponent Main.component)
+-- main = JSaddle.Wasm.run app
+
+
+-- | WASM export, required when compiling w/ the WASM backend.
+#ifdef WASM
+foreign export javascript "hs_start" main :: IO ()
+#endif
+
+-- | `defaultComponent` takes as arguments the initial model, update function, view function
+component :: Component name Model Action
+component = defaultComponent emptyModel updateModel viewModel
+
+-- | Empty application state
+emptyModel :: Model
+emptyModel = Model 0
+
+-- | Updates model, optionally introduces side effects
+updateModel :: Action -> Effect Model Action
+updateModel = \case
+  AddOne        -> counter += 1
+  SubtractOne   -> counter -= 1
+  SayHelloWorld -> io_ $ do
+    consoleLog "Hello World"
+    alert "Hello World"
+
+-- | Constructs a virtual DOM from a model
+viewModel :: Model -> View Action
+viewModel x = div_ []
+  [ button_ [ onClick AddOne ] [ text "+" ]
+  , text $ ms (x ^. counter)
+  , button_ [ onClick SubtractOne ] [ text "-" ]
+  , button_ [ onClick SayHelloWorld ] [ text "Alert Hello World!" ]
+  ]
+
